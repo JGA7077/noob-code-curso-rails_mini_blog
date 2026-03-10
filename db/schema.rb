3f8e04d6ff77895d81f6acdfe2a851ecbf77b56e
João Gabriel <joaogabriel-al@hotmail.com>
2026-03-10 09:19:41 -0300

    aula 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2026_03_06_192045) do
+ActiveRecord::Schema.define(version: 2026_03_09_174938) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
